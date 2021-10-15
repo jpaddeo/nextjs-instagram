112652214be488b78383bb0bdafba97a8ff7722b
jpaddeo <jpaddeo@gmail.com>
2021-10-15 12:53:25 -0300

    Fixing session error

--- components/posts/Post.js
+++ components/posts/Post.js
@@ -81,7 +81,7 @@ function Post({ id, username, userImg, img, caption }) {
 
   useEffect(() => {
     setHasLiked(
-      likes.findIndex((like) => like.id === session.user?.uid) !== -1
+      likes.findIndex((like) => like.id === session?.user?.uid) !== -1
     );
   }, [likes]);
 
